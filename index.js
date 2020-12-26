81465dcbfea84f7d377930edaa7d82374e6db2c1
N Oliver <github@pixnbits.org>
2020-12-26 10:55:33 -0700

    fix(index): input to output connection keep the output enabled on input reconnection

--- index.js
+++ index.js
@@ -2,43 +2,34 @@
 const inputRepresentationEmitter = require('./src/input-representation');
 const { addReadyListener, updateOutput } = require('./src/output');
 
-let enableOutput = false;
+inputRepresentationEmitter
+  .on('device', ({ name }) => {
+    console.log(`found input device ${name}`);
+  })
+  .on('error', (err) => {
+    console.error('input device error:', err);
+    // TODO: differentiate between input disconnect errors and others
+    // only device disconnect (out of range?) errors seen so far
+    // reset the output to avoid zombie/sleepwalking based on the last known position
+    updateOutput({
+      steering: {
+        angle: 0,
+      },
+      throttle: {
+        direction: 'S',
+        magnitude: 0,
+      },
+    });
+  });
 
 addReadyListener((err) => {
   if (err) {
     console.error('output device error', err);
-    process.exitCode = 3;
+    process.exitCode = 1;
     throw err;
   }
-  enableOutput = true;
-});
 
-inputRepresentationEmitter.on('error', (err) => {
-  console.error('input device error', err);
-  // TODO: differentiate between input disconnect errors and others
-  // only device disconnect (out of range?) errors seen so far
-  // reset the output to avoid zombie/sleepwalking based on the last known position
-  enableOutput = false;
-  updateOutput({
-    steering: {
-      angle: 0,
-    },
-    throttle: {
-      direction: 'S',
-      magnitude: 0,
-    },
-  })
-});
-
-inputRepresentationEmitter.once('device', ({ name }) => {
-  console.log(`reading from ${name}`);
-});
-
-inputRepresentationEmitter.on('representation', (inputRepresentation) => {
-  if (enableOutput) {
-    updateOutput(inputRepresentation);
-  }
-
-  // dashboard(inputRepresentation);
-  // console.log(enableOutput, inputRepresentation);
+  console.log('output ready');
+  inputRepresentationEmitter.on('representation', updateOutput);
+  console.log('input reader and output connected');
 });
